bbfcdb830ce8c97bec114c8eb5c7dad59b2b4acd
victor@hungrydigital.com | Victor Rodrigues <victor@hungrydigital.com>
2024-10-01 19:05:45 -0300

    Remoção da página de links e reset do portifolio

--- js/script.js
+++ js/script.js
@@ -12,33 +12,33 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop - 150;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-//         if(top >= offset && top < offset + height) {
-//             navLinks.forEach(links => {
-//                 links.classList.remove('active');
-//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-//             });
-//         };
-//     });
-
-
-// /*========== sticky navbar ==========*/
-// let header = document.querySelector('.header');
-
-// header.classList.toggle('sticky', window.scrollY > 50);
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
 
 
-// /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
-// menuIcon.classList.remove('bx-x');
-// navbar.classList.remove('active');
+/*========== sticky navbar ==========*/
+let header = document.querySelector('.header');
 
-// };
+header.classList.toggle('sticky', window.scrollY > 50);
+
+
+/*========== remove menu icon navbar when click navbar link (scroll) ==========*/
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+
+};
 
 
 /*========== swiper ==========*/
